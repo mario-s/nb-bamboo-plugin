f3badb542eed64601e6b305f1c014922aa01f707
Mario-S <spindizzy@outlook.com>
2015-11-13 12:42:16 +0100

    cbuttun is using an action instead of actionlistener

--- src/main/java/org/netbeans/bamboo/ui/wizard/InstanceDialog.java
+++ src/main/java/org/netbeans/bamboo/ui/wizard/InstanceDialog.java
@@ -3,7 +3,7 @@ package org.netbeans.bamboo.ui.wizard;
 import java.awt.Dialog;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.AbstractAction;
 import javax.swing.JButton;
 import org.netbeans.bamboo.BambooManager;
 import org.openide.DialogDescriptor;
@@ -13,45 +13,57 @@ import org.openide.DialogDisplayer;
 import org.openide.NotifyDescriptor;
 
 @Messages({
-  "LBL_DIALOG=Add Bamboo Instance",
-  "TXT_ADD=OK"
+    "LBL_DIALOG=Add Bamboo Instance",
+    "TXT_ADD=OK"
 })
 public class InstanceDialog extends DialogDescriptor {
 
-  private Dialog dialog;
+    private Dialog dialog;
 
-  private InstancePropertiesForm form;
+    private final InstancePropertiesForm form;
 
-  public InstanceDialog() {
-    this(new InstancePropertiesForm());
-  }
 
-  public InstanceDialog(final InstancePropertiesForm form) {
-    super(form, LBL_DIALOG());
-    this.form = form;
+    public InstanceDialog() {
+        this(new InstancePropertiesForm());
+    }
 
-    final JButton addButton = new JButton(TXT_ADD());
-    addButton.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(final ActionEvent e) {
-        final String name = form.getInstanceName();
-        final String url = form.getInstanceUrl();
-        BambooManager.addInstance(name, url);
-        EventQueue.invokeLater(new Runnable() {
-          @Override
-          public void run() {
-            dialog.dispose();
-          }
-        });
-      }
-    });
+    private InstanceDialog(final InstancePropertiesForm form) {
+        super(form, LBL_DIALOG());
+        this.form = form;
+        initializeGui();
+    }
 
-    setOptions(new Object[]{addButton, NotifyDescriptor.CANCEL_OPTION});
-    setClosingOptions(new Object[]{NotifyDescriptor.CANCEL_OPTION});
-  }
+    private void initializeGui() {
+        this.dialog = DialogDisplayer.getDefault().createDialog(this);
+        
+        AbstractAction action = new AddAction();
+        JButton addButton = new JButton(action);
 
-  public void show() {
-    dialog = DialogDisplayer.getDefault().createDialog(this);
-    dialog.setVisible(true);
-  }
+        setOptions(new Object[]{addButton, NotifyDescriptor.CANCEL_OPTION});
+        setClosingOptions(new Object[]{NotifyDescriptor.CANCEL_OPTION});
+    }
+
+    public void show() {
+        dialog.setVisible(true);
+    }
+
+    private class AddAction extends AbstractAction {
+
+        AddAction() {
+            super(TXT_ADD());
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            final String name = form.getInstanceName();
+            final String url = form.getInstanceUrl();
+            BambooManager.addInstance(name, url);
+            EventQueue.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    dialog.dispose();
+                }
+            });
+        }
+    }
 }
