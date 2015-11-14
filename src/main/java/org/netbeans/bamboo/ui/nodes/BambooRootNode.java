60294c210d842193c5b9740c942b3e381fdc992e
Mario-S <spindizzy@outlook.com>
2015-11-14 11:28:22 +0100

    moved node before hudson builders

--- src/main/java/org/netbeans/bamboo/ui/nodes/BambooRootNode.java
+++ src/main/java/org/netbeans/bamboo/ui/nodes/BambooRootNode.java
@@ -1,19 +1,12 @@
 package org.netbeans.bamboo.ui.nodes;
 
-import java.util.List;
 import javax.swing.Action;
 import org.netbeans.api.core.ide.ServicesTabNodeRegistration;
 import org.netbeans.bamboo.LookupProvider;
-import org.netbeans.bamboo.model.BambooInstance;
 import org.netbeans.bamboo.ui.actions.AddInstanceAction;
 import static org.netbeans.bamboo.ui.nodes.Bundle.*;
 import org.openide.nodes.AbstractNode;
-import org.openide.nodes.ChildFactory;
 import org.openide.nodes.Children;
-import org.openide.nodes.Node;
-import org.openide.util.Lookup;
-import org.openide.util.LookupEvent;
-import org.openide.util.LookupListener;
 import org.openide.util.NbBundle.Messages;
 
 @ServicesTabNodeRegistration(
@@ -21,7 +14,7 @@ import org.openide.util.NbBundle.Messages;
         displayName = "#LBL_BambooNode",
         shortDescription = "#TIP_BambooNode",
         iconResource = BambooRootNode.ICON_BASE,
-        position = 600)
+        position = 450)
 @Messages({
     "LBL_BambooNode=Bamboo Builders",
     "TIP_BambooNode=Bamboo continuous integration servers."
