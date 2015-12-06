eff1fd962433862b9ec6aa729e678436a518b473
Mario-S <spindizzy@outlook.com>
2015-12-06 21:51:45 +0100

    cleanup in test

--- src/test/java/org/netbeans/modules/bamboo/ui/wizard/AddActionTest.java
+++ src/test/java/org/netbeans/modules/bamboo/ui/wizard/AddActionTest.java
@@ -1,24 +1,15 @@
 package org.netbeans.modules.bamboo.ui.wizard;
 
 import java.awt.Dialog;
-import java.awt.event.ActionEvent;
-import java.lang.reflect.InvocationTargetException;
-import javax.swing.SwingUtilities;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
 import static org.mockito.BDDMockito.given;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.verify;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.netbeans.modules.bamboo.BambooInstanceConstants;
-import org.netbeans.modules.bamboo.BambooManager;
-import org.netbeans.modules.bamboo.model.BambooInstance;
-import org.openide.util.Lookup;
-import org.openide.util.LookupListener;
 
 /**
  *
@@ -35,13 +26,8 @@ public class AddActionTest {
     @Mock
     private InstancePropertiesForm form;
     
-    @Mock
-    private LookupListener listener;
-
     private AddAction classUnderTest;
     
-    private Lookup.Result<BambooInstance> result;
-
     @Before
     public void setUp() {
         classUnderTest = new AddAction(dialog, form) {
@@ -50,11 +36,6 @@ public class AddActionTest {
                 dialog.dispose();
             }
         };
-        
-        result = BambooManager.Instance.getLookup().lookupResult(
-                BambooInstance.class);
-        result.addLookupListener(listener);
-        
         
         given(form.getInstanceName()).willReturn(NAME);
     }
