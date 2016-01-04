4fbfb8d1966e41358cd21f0b7eaae0a9f53c4721
Mario-S <spindizzy@outlook.com>
2016-01-04 09:47:43 +0100

    updated client

--- src/main/java/org/netbeans/modules/bamboo/rest/BambooRestClient.java
+++ src/main/java/org/netbeans/modules/bamboo/rest/BambooRestClient.java
@@ -1,6 +1,15 @@
 package org.netbeans.modules.bamboo.rest;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+
+
 public class BambooRestClient {
+    private final String BAMBOO_RESULT_URL =
+        "{serverUrl}/rest/api/latest/result/{buildKey}.json?os_authType=basic&os_username={username}&os_password={password}";
+    private final String BAMBOO_PLAN_URL =
+        "{serverUrl}/rest/api/latest/plan/{buildKey}.json?os_authType=basic&os_username={username}&os_password={password}";
+
     private String serverUrl;
 
     public BambooRestClient(final String serverUrl) {
@@ -9,6 +18,8 @@ public class BambooRestClient {
 
     public ResultsResponse getResultsResponse() {
         ResultsResponse response = new ResultsResponse();
+        Client client = ClientBuilder.newClient();
+        String entity = client.target(serverUrl).path("").request().get(String.class);
 
         return response;
     }
