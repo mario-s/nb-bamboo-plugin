162bceaccd5fc8b319edb930fe87efd69c31ceb0
Mario-S <spindizzy@outlook.com>
2016-01-10 14:20:34 +0100

    added getter & setter for username/password

--- src/main/java/org/netbeans/modules/bamboo/InstanceValues.java
+++ src/main/java/org/netbeans/modules/bamboo/InstanceValues.java
@@ -11,6 +11,10 @@ public class InstanceValues {
     private String url;
     
     private int syncTime;
+    
+    private String username;
+    
+    private char [] password;
 
     public String getName() {
         return name;
@@ -35,6 +39,20 @@ public class InstanceValues {
     public void setSyncTime(int syncTime) {
         this.syncTime = syncTime;
     }
-    
-    
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public char[] getPassword() {
+        return password;
+    }
+
+    public void setPassword(char[] password) {
+        this.password = password;
+    }
 }
