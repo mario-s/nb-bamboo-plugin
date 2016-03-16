9145a1e90380a54481eedf120fdf8fcf14393618
Mario-S <spindizzy@outlook.com>
2016-03-16 10:45:24 +0100

    updated web target

--- src/main/java/org/netbeans/modules/bamboo/rest/BambooRestClient.java
+++ src/main/java/org/netbeans/modules/bamboo/rest/BambooRestClient.java
@@ -1,31 +1,41 @@
 package org.netbeans.modules.bamboo.rest;
 
+import org.netbeans.modules.bamboo.glue.InstanceValues;
+
+import org.openide.util.lookup.ServiceProvider;
 
 import java.util.logging.Logger;
 
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
-import org.netbeans.modules.bamboo.glue.InstanceValues;
-import org.openide.util.lookup.ServiceProvider;
+
 
 @ServiceProvider(service = BambooInstanceAccessable.class)
-public class BambooRestClient implements BambooInstanceAccessable{
-    private static final String AUTHEN =
-        "?os_authType=basic&os_username={username}&os_password={password}";
+public class BambooRestClient implements BambooInstanceAccessable {
+    private static final String AUTH_TYPE = "os_authType";
+    private static final String BASIC = "basic";
+    private static final String USER = "os_username";
+    private static final String PASS = "os_password";
+
     private static final String REST_API = "/rest/api/latest";
     private static final String ALL_PLANS = REST_API + "/plan";
     private final String PLAN = ALL_PLANS + "/{buildKey}.json";
     private final String RESULT = REST_API + "/result/{buildKey}.json";
 
-    private WebTarget target(String serverUrl) {
-        return ClientBuilder.newClient().target(serverUrl);
+    private WebTarget target(final InstanceValues values, final String path) {
+        String url = values.getUrl();
+        String user = values.getName();
+        String password = String.valueOf(values.getPassword());
+
+        return ClientBuilder.newClient().target(url).path(path).queryParam(AUTH_TYPE, BASIC)
+                            .queryParam(USER, user).queryParam(PASS, password);
     }
 
     @Override
-    public Plans getPlans(InstanceValues values) {
+    public Plans getPlans(final InstanceValues values) {
         Plans plans = new Plans();
 
-        String entity = target(values.getUrl()).path(ALL_PLANS).request().get(String.class);
+        String entity = target(values, ALL_PLANS).request().get(String.class);
 
         Logger.getLogger(getClass().getName()).fine(entity);
 
@@ -33,10 +43,10 @@ public class BambooRestClient implements BambooInstanceAccessable{
     }
 
     @Override
-    public ResultsResponse getResultsResponse(InstanceValues values) {
+    public ResultsResponse getResultsResponse(final InstanceValues values) {
         ResultsResponse response = new ResultsResponse();
 
-        String entity = target(values.getUrl()).path(RESULT).request().get(String.class);
+        String entity = target(values, RESULT).request().get(String.class);
 
         Logger.getLogger(getClass().getName()).fine(entity);
 
