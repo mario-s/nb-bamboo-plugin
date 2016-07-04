086f9db42f2744335ee635088d6f6de7efdb2b87
Mario-S <spindizzy@outlook.com>
2016-07-04 10:21:41 +0200

    extended mock

--- src/test/java/org/netbeans/modules/bamboo/rest/MockRestClient.java
+++ src/test/java/org/netbeans/modules/bamboo/rest/MockRestClient.java
@@ -1,27 +1,32 @@
 package org.netbeans.modules.bamboo.rest;
 
-import java.util.ArrayList;
 import org.netbeans.modules.bamboo.glue.InstanceValues;
+
 import org.openide.util.lookup.ServiceProvider;
 
+import java.util.ArrayList;
+
+
 /**
- *
  * @author spindizzy
  */
 @ServiceProvider(service = BambooInstanceAccessable.class)
-public class MockRestClient implements BambooInstanceAccessable{
-
+public class MockRestClient implements BambooInstanceAccessable {
     @Override
-    public Plans getPlans(InstanceValues values) {
+    public Plans getPlans(final InstanceValues values) {
         Plans plans = new Plans();
         plans.setPlans(new ArrayList<>());
         plans.getPlans().add(new Plan());
+
         return plans;
     }
 
     @Override
-    public ResultsResponse getResultsResponse(InstanceValues values) {
-        throw new UnsupportedOperationException("Not supported yet."); 
+    public ResultsResponse getResultsResponse(final InstanceValues values) {
+        Results.Builder resBuilder = new Results.Builder();
+        Results results = resBuilder.addResult(new Result()).build();
+        ResultsResponse.Builder respBuilder = new ResultsResponse.Builder();
+
+        return respBuilder.results(results).build();
     }
-    
 }
