cce0e7cb36b2d4c72e982d2400785b157420d09e
Mario-S <spindizzy@outlook.com>
2016-07-04 16:55:53 +0200

    renamed field for correct access

--- src/main/java/org/netbeans/modules/bamboo/rest/Plans.java
+++ src/main/java/org/netbeans/modules/bamboo/rest/Plans.java
@@ -1,19 +1,16 @@
 package org.netbeans.modules.bamboo.rest;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import java.util.List;
 
 
 public class Plans extends Metrics {
-    @JsonProperty(value = "plan")
-    private List<Plan> plans;
+    private List<Plan> plan;
 
-    public List<Plan> getPlans() {
-        return plans;
+    public List<Plan> getPlan() {
+        return plan;
     }
 
-    public void setPlans(final List<Plan> plans) {
-        this.plans = plans;
+    public void setPlan(final List<Plan> plans) {
+        this.plan = plans;
     }
 }
