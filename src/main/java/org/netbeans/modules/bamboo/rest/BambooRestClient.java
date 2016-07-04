6c07c9c7a572dfbf942523aa14ca8040c0de35f1
Mario-S <spindizzy@outlook.com>
2016-07-04 16:45:57 +0200

    added logging for request/ response

--- src/main/java/org/netbeans/modules/bamboo/rest/BambooRestClient.java
+++ src/main/java/org/netbeans/modules/bamboo/rest/BambooRestClient.java
@@ -3,6 +3,8 @@ package org.netbeans.modules.bamboo.rest;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 
+import org.glassfish.jersey.logging.LoggingFeature;
+
 import org.netbeans.modules.bamboo.glue.InstanceValues;
 
 import org.openide.util.lookup.ServiceProvider;
@@ -10,10 +12,12 @@ import org.openide.util.lookup.ServiceProvider;
 import java.util.Optional;
 import static java.util.Optional.empty;
 import static java.util.Optional.of;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Feature;
 
 
 /**
@@ -33,9 +37,11 @@ public class BambooRestClient implements BambooInstanceAccessable {
     private static final String PLAN = ALL_PLANS + "/{buildKey}.json";
 
     private final Logger log;
+    private final Feature logFeature;
 
     public BambooRestClient() {
         this.log = Logger.getLogger(getClass().getName());
+        this.logFeature = new LoggingFeature(log, Level.INFO, null, null);
     }
 
     private Optional<WebTarget> newTarget(final InstanceValues values, final String path) {
@@ -48,15 +54,17 @@ public class BambooRestClient implements BambooInstanceAccessable {
                 ArrayUtils.isNotEmpty(chars)) {
             String password = String.valueOf(chars);
 
-            opt = of(BambooRestClient.this.newTarget(url).path(REST_API).path(path).queryParam(AUTH_TYPE, BASIC)
-                    .queryParam(USER, user).queryParam(PASS, password));
+            opt = of(
+                    BambooRestClient.this.newTarget(url).path(REST_API).path(path).queryParam(
+                        AUTH_TYPE,
+                        BASIC).queryParam(USER, user).queryParam(PASS, password));
         }
 
         return opt;
     }
 
     WebTarget newTarget(final String url) {
-        return ClientBuilder.newClient().target(url);
+        return ClientBuilder.newClient().register(logFeature).target(url);
     }
 
     private <T> T request(final WebTarget target, final Class<T> clazz) {
