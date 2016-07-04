38fda1ffa52fdc03f43604a5874607d541c0035c
Mario-S <spindizzy@outlook.com>
2016-07-04 15:38:59 +0200

    added check for exception

--- src/main/java/org/netbeans/modules/bamboo/model/BambooInstanceProperties.java
+++ src/main/java/org/netbeans/modules/bamboo/model/BambooInstanceProperties.java
@@ -1,73 +1,85 @@
 package org.netbeans.modules.bamboo.model;
 
+import org.netbeans.modules.bamboo.glue.InstanceValues;
+import static org.netbeans.modules.bamboo.model.BambooInstanceConstants.*;
+
+import org.openide.util.Exceptions;
+import org.openide.util.RequestProcessor;
+
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
-import org.netbeans.modules.bamboo.glue.InstanceValues;
-import static org.netbeans.modules.bamboo.model.BambooInstanceConstants.*;
-import org.openide.util.Exceptions;
-import org.openide.util.RequestProcessor;
+
 
 /**
- * Instance properties for Bamboo instance
+ * Instance properties for Bamboo instance.
  */
 public class BambooInstanceProperties extends HashMap<String, String> {
+    /** Use serialVersionUID for interoperability. */
+    private static final long serialVersionUID = 1L;
 
-    private static final RequestProcessor RP = new RequestProcessor(
-            BambooInstanceProperties.class);
+    private static final RequestProcessor RP = new RequestProcessor(BambooInstanceProperties.class);
+
+    private static final Logger LOG = Logger.getLogger(BambooInstanceProperties.class.getName());
 
     private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
 
     private final Preferences preferences;
 
-    public BambooInstanceProperties(Preferences preferences) {
+    public BambooInstanceProperties(final Preferences preferences) {
         this.preferences = preferences;
     }
 
-    public void copyProperties(InstanceValues values) {
+    public void copyProperties(final InstanceValues values) {
         put(INSTANCE_NAME, values.getName());
         put(INSTANCE_URL, values.getUrl());
         put(INSTANCE_SYNC, Integer.toString(values.getSyncInterval()));
         put(INSTANCE_USER, values.getUsername());
-        put(INSTANCE_PASSWORD, Encrypter.getInstance().encrypt(
-                values.getPassword()));
+        put(INSTANCE_PASSWORD, Encrypter.getInstance().encrypt(values.getPassword()));
     }
 
     @Override
-    public final synchronized String put(String key, String value) {
+    public final synchronized String put(final String key, final String value) {
         String o = super.put(key, value);
         pcs.firePropertyChange(key, o, value);
+
         if (key.equals(INSTANCE_NAME)) {
             loadPreferences();
         }
+
         updatePreferences(key);
+
         return o;
     }
 
     @Override
-    public synchronized String remove(Object key) {
+    public synchronized String remove(final Object key) {
         String o = super.remove((String) key);
         pcs.firePropertyChange((String) key, o, null);
         updatePreferences((String) key);
+
         return o;
     }
 
     public final boolean isPersisted() {
         String pers = get(INSTANCE_PERSISTED);
-        return pers == null || TRUE.equals(pers);
+
+        return (pers == null) || TRUE.equals(pers);
     }
 
-    public void addPropertyChangeListener(PropertyChangeListener l) {
+    public void addPropertyChangeListener(final PropertyChangeListener l) {
         pcs.addPropertyChangeListener(l);
     }
 
-    public void removePropertyChangeListener(PropertyChangeListener l) {
+    public void removePropertyChangeListener(final PropertyChangeListener l) {
         pcs.removePropertyChangeListener(l);
     }
 
@@ -75,30 +87,36 @@ public class BambooInstanceProperties extends HashMap<String, String> {
         return Arrays.asList(pcs.getPropertyChangeListeners());
     }
 
-    public static List<String> split(String prop) {
-        if (prop != null && prop.trim().length() > 0) {
+    public static List<String> split(final String prop) {
+        if ((prop != null) && (prop.trim().length() > 0)) {
             String[] escaped = prop.split("(?<!/)/(?!/)");
             List<String> list = new ArrayList<>(escaped.length);
+
             for (String e : escaped) {
                 list.add(e.replace("//", "/"));
             }
+
             return list;
         } else {
             return Collections.<String>emptyList();
         }
     }
 
-    public static String join(List<String> pieces) {
+    public static String join(final List<String> pieces) {
         StringBuilder b = new StringBuilder();
+
         for (String piece : pieces) {
-            assert !piece.startsWith("/")
-                    && !piece.endsWith("/") : piece;
+            assert !piece.startsWith("/") && !piece.endsWith("/") : piece;
+
             String escaped = piece.replace("/", "//");
+
             if (b.length() > 0) {
                 b.append('/');
             }
+
             b.append(escaped);
         }
+
         return b.toString();
     }
 
@@ -107,6 +125,7 @@ public class BambooInstanceProperties extends HashMap<String, String> {
      */
     public Preferences getPreferences() {
         String nodeName = getNodeName();
+
         if (nodeName != null) {
             return preferences.node(nodeName);
         } else {
@@ -119,6 +138,7 @@ public class BambooInstanceProperties extends HashMap<String, String> {
      */
     private boolean hasPreferences() {
         String nodeName = getNodeName();
+
         if (nodeName != null) {
             try {
                 return preferences.nodeExists(nodeName);
@@ -135,17 +155,18 @@ public class BambooInstanceProperties extends HashMap<String, String> {
      */
     private String getNodeName() {
         String name = get(INSTANCE_NAME);
-        if (name != null && !name.isEmpty()) {
+
+        if ((name != null) && !name.isEmpty()) {
             return simplifyServerLocation(name, true);
         } else {
             return null;
         }
     }
 
-    private String simplifyServerLocation(String name, boolean forKey) {
+    private String simplifyServerLocation(final String name, final boolean forKey) {
         // http://deadlock.netbeans.org/hudson/ => deadlock.netbeans.org_hudson
-        String display = name.replaceFirst("https?://", "").replaceFirst("/$",
-                "");
+        String display = name.replaceFirst("https?://", "").replaceFirst("/$", "");
+
         return forKey ? display.replaceAll("[/:]", "_") : display;
     }
 
@@ -154,18 +175,20 @@ public class BambooInstanceProperties extends HashMap<String, String> {
      */
     private void updatePreferences(final String... keys) {
         RP.post(() -> {
-            Preferences prefs = getPreferences();
-            if (prefs != null) {
-                for (String key : keys) {
-                    String val = get(key);
-                    if (val == null) {
-                        prefs.remove(key);
-                    } else {
-                        prefs.put(key, val);
+                Preferences prefs = getPreferences();
+
+                if (prefs != null) {
+                    for (String key : keys) {
+                        String val = get(key);
+
+                        if (val == null) {
+                            prefs.remove(key);
+                        } else {
+                            prefs.put(key, val);
+                        }
                     }
                 }
-            }
-        });
+            });
     }
 
     /**
@@ -173,29 +196,41 @@ public class BambooInstanceProperties extends HashMap<String, String> {
      */
     private void loadPreferences() {
         RP.post(() -> {
-            if (hasPreferences()) {
-                Preferences prefs = getPreferences();
-                if (prefs != null) {
-                    try {
-                        String[] keys = prefs.keys();
-                        for (String key : keys) {
-                            if (INSTANCE_NAME.equals(key)
-                                    || INSTANCE_URL.equals(key)) {
-                                continue;
+                if (hasPreferences()) {
+                    Preferences prefs = getPreferences();
+
+                    if (prefs != null) {
+                        try {
+                            String[] keys = prefs.keys();
+
+                            for (String key : keys) {
+                                if (INSTANCE_NAME.equals(key) || INSTANCE_URL.equals(key)) {
+                                    continue;
+                                }
+
+                                put(prefs, key);
                             }
-                            String val = prefs.get(key, null);
-                            if (val != null) {
-                                if (INSTANCE_PASSWORD.equals(key)) {
-                                    val = Encrypter.getInstance().decrypt(val);
-                                } 
-                                put(key, val);
-                            }
+                        } catch (BackingStoreException ex) {
+                            Exceptions.printStackTrace(ex);
                         }
-                    } catch (BackingStoreException ex) {
-                        Exceptions.printStackTrace(ex);
                     }
                 }
+            });
+    }
+
+    private void put(final Preferences prefs, final String key) {
+        try {
+            String val = prefs.get(key, null);
+
+            if (val != null) {
+                if (INSTANCE_PASSWORD.equals(key)) {
+                    val = Encrypter.getInstance().decrypt(val);
+                }
+
+                put(key, val);
             }
-        });
+        } catch (IllegalStateException ex) {
+            LOG.info(ex.getMessage());
+        }
     }
 }
