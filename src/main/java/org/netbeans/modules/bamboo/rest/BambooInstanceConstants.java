4ede21545588a6337293aae47f5b9b4524f26a8f
Mario-S <spindizzy@outlook.com>
2016-07-06 13:01:44 +0200

    changed scope of class

--- src/main/java/org/netbeans/modules/bamboo/rest/BambooInstanceConstants.java
+++ src/main/java/org/netbeans/modules/bamboo/rest/BambooInstanceConstants.java
@@ -4,39 +4,39 @@ package org.netbeans.modules.bamboo.rest;
  *
  * @author spindizzy
  */
-public final class BambooInstanceConstants {
+final class BambooInstanceConstants {
 
     private BambooInstanceConstants() {}
     
-    public static final String INSTANCE_NAME = "name";
+    static final String INSTANCE_NAME = "name";
     
-    public static final String INSTANCE_URL = "url";
+    static final String INSTANCE_URL = "url";
     
-    public static final String INSTANCE_SYNC = "sync_time"; 
+    static final String INSTANCE_SYNC = "sync_time"; 
     
-    public static final String INSTANCE_USER = "username";
+    static final String INSTANCE_USER = "username";
     
-    public static final String INSTANCE_PASSWORD = "password";
+    static final String INSTANCE_PASSWORD = "password";
 
     /**
      * preferred jobs for the instance, list of job names, separated by |
      */
-    public static final String INSTANCE_PREF_JOBS = "pref_jobs";
+    static final String INSTANCE_PREF_JOBS = "pref_jobs";
 
     /**
      * Nonsalient jobs for the instance, list of job names, separated by |
      */
-    public static final String INSTANCE_SUPPRESSED_JOBS = "suppressed_jobs";
+    static final String INSTANCE_SUPPRESSED_JOBS = "suppressed_jobs";
 
-    public static final String INSTANCE_PERSISTED = "persisted";
+    static final String INSTANCE_PERSISTED = "persisted";
 
     /**
      * True value, e.g. for {@link #INSTANCE_PERSISTED} key.
      */
-    public static final String TRUE = "true";
+    static final String TRUE = "true";
     /**
      * False value, e.g. for {@link #INSTANCE_PERSISTED} key.
      */
-    public static final String FALSE = "false";
+    static final String FALSE = "false";
     
 }
