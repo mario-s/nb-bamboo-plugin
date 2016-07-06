b802599e5ab79df3f64bbd4aa7e2b856a1d42f39
Mario-S <spindizzy@outlook.com>
2016-07-06 12:54:23 +0200

    added lombok annotations

--- src/main/java/org/netbeans/modules/bamboo/glue/DefaultInstanceValues.java
+++ src/main/java/org/netbeans/modules/bamboo/glue/DefaultInstanceValues.java
@@ -1,16 +1,21 @@
 package org.netbeans.modules.bamboo.glue;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * A wrapper for all parameters to be used to create a new BambooInstance.
  *
  * @author spindizzy
  */
+@Getter
+@Setter
 public class DefaultInstanceValues implements InstanceValues {
     private String name;
 
     private String url;
 
-    private int syncTime;
+    private int syncInterval;
 
     private String username;
 
@@ -22,53 +27,9 @@ public class DefaultInstanceValues implements InstanceValues {
     public DefaultInstanceValues(final InstanceValues other) {
         this.name = other.getName();
         this.url = other.getUrl();
-        this.syncTime = other.getSyncInterval();
+        this.syncInterval = other.getSyncInterval();
         this.username = other.getUsername();
         this.password = other.getPassword();
     }
 
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    public void setName(final String name) {
-        this.name = name;
-    }
-
-    @Override
-    public int getSyncInterval() {
-        return syncTime;
-    }
-
-    public void setSyncInterval(final int syncTime) {
-        this.syncTime = syncTime;
-    }
-
-    @Override
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(final String url) {
-        this.url = url;
-    }
-
-    @Override
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(final String username) {
-        this.username = username;
-    }
-
-    @Override
-    public char[] getPassword() {
-        return password;
-    }
-
-    public void setPassword(final char[] password) {
-        this.password = password;
-    }
 }
