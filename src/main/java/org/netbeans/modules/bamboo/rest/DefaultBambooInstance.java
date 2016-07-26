197d3b6bd39d7a6ab1c4784af8c86955c74079ac
Mario-S <spindizzy@outlook.com>
2016-07-26 18:17:56 +0200

    updated properties after loading

--- src/main/java/org/netbeans/modules/bamboo/rest/DefaultBambooInstance.java
+++ src/main/java/org/netbeans/modules/bamboo/rest/DefaultBambooInstance.java
@@ -1,34 +1,33 @@
 package org.netbeans.modules.bamboo.rest;
 
-import org.netbeans.modules.bamboo.glue.DefaultInstanceValues;
-import org.netbeans.modules.bamboo.rest.model.Plan;
-import org.netbeans.modules.bamboo.glue.PlansProvideable;
-import java.util.List;
-import org.netbeans.modules.bamboo.glue.InstanceValues;
-
-import java.util.prefs.Preferences;
 import lombok.Getter;
 import lombok.Setter;
-import org.apache.commons.lang3.StringUtils;
+
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
+import org.netbeans.modules.bamboo.glue.DefaultInstanceValues;
+import org.netbeans.modules.bamboo.glue.InstanceValues;
 import org.netbeans.modules.bamboo.glue.ResultsProvideable;
+import org.netbeans.modules.bamboo.rest.model.Plan;
 import org.netbeans.modules.bamboo.rest.model.Result;
+
+import java.util.List;
+import java.util.prefs.Preferences;
+
 
 /**
  * @author spindizzy
  */
 @Getter
 @Setter
-public final class DefaultBambooInstance extends DefaultInstanceValues implements ResultsProvideable {
+public final class DefaultBambooInstance extends DefaultInstanceValues
+    implements ResultsProvideable {
+    /** Use serialVersionUID for interoperability. */
+    private static final long serialVersionUID = 1L;
 
     private List<Plan> plans;
-    
-    private List<Result> results;
 
-    /**
-     * Use serialVersionUID for interoperability.
-     */
-    private static final long serialVersionUID = 1L;
+    private List<Result> results;
 
     private BambooInstanceProperties properties;
 
@@ -51,12 +50,19 @@ public final class DefaultBambooInstance extends DefaultInstanceValues implement
 
     public void setProperties(final BambooInstanceProperties properties) {
         this.properties = properties;
-        updateFields(properties);
+        copyProperties(properties);
     }
 
-    private void updateFields(final BambooInstanceProperties props) throws NumberFormatException {
+    private void copyProperties(final BambooInstanceProperties props) throws NumberFormatException {
         setName(props.get(BambooInstanceConstants.INSTANCE_NAME));
         setUrl(props.get(BambooInstanceConstants.INSTANCE_URL));
+        setUsername(props.get(BambooInstanceConstants.INSTANCE_USER));
+
+        String passwd = props.get(BambooInstanceConstants.INSTANCE_PASSWORD);
+
+        if (isNotBlank(passwd)) {
+            setPassword(passwd.toCharArray());
+        }
 
         String syncProp = props.get(BambooInstanceConstants.INSTANCE_SYNC);
 
