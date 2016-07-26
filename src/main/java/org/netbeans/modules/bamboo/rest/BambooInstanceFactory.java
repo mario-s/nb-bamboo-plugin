1b3c120301a052925af561204846bbfcebb4fec5
Mario-S <spindizzy@outlook.com>
2016-07-26 18:16:59 +0200

    removed call to set props, since this destroyed the password

--- src/main/java/org/netbeans/modules/bamboo/rest/BambooInstanceFactory.java
+++ src/main/java/org/netbeans/modules/bamboo/rest/BambooInstanceFactory.java
@@ -1,20 +1,21 @@
 package org.netbeans.modules.bamboo.rest;
 
-import org.netbeans.modules.bamboo.rest.model.PlansResponse;
-import java.util.Collection;
 import org.netbeans.modules.bamboo.glue.InstanceValues;
-import java.util.prefs.Preferences;
 import org.netbeans.modules.bamboo.glue.ResultsProvideable;
 import org.netbeans.modules.bamboo.rest.model.Plans;
+import org.netbeans.modules.bamboo.rest.model.PlansResponse;
+
 import org.openide.util.Lookup;
 import org.openide.util.lookup.ServiceProvider;
 
+import java.util.Collection;
+import java.util.prefs.Preferences;
+
+
 /**
- *
  */
 @ServiceProvider(service = BambooInstanceProduceable.class)
 public class BambooInstanceFactory implements BambooInstanceProduceable {
-
     private BambooServiceAccessable instanceAccessor;
 
     public BambooInstanceFactory() {
@@ -22,22 +23,23 @@ public class BambooInstanceFactory implements BambooInstanceProduceable {
     }
 
     private void initClient() {
-        Collection<? extends BambooServiceAccessable> services = Lookup.getDefault().lookupAll(BambooServiceAccessable.class);
+        Collection<? extends BambooServiceAccessable> services = Lookup.getDefault().lookupAll(
+                BambooServiceAccessable.class);
 
         // simply take the first one, in test environment it is the mock client
         this.instanceAccessor = services.iterator().next();
     }
 
     @Override
-    public ResultsProvideable create(InstanceValues values) {
+    public ResultsProvideable create(final InstanceValues values) {
         DefaultBambooInstance instance = new DefaultBambooInstance(values);
         BambooInstanceProperties props = new BambooInstanceProperties(instancesPrefs());
         props.copyProperties(instance);
-        instance.setProperties(props);
 
         //TODO use results
         PlansResponse all = instanceAccessor.getAllPlans(instance);
         Plans plans = all.getPlans();
+
         if (plans != null) {
             instance.setPlans(plans.getPlan());
         }
