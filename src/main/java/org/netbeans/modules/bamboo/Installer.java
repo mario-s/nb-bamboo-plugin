705e2d7e896e4554f625f22049a5a802662755fe
Mario-S <spindizzy@outlook.com>
2016-07-26 18:18:47 +0200

    installer is loading plan from service

--- src/main/java/org/netbeans/modules/bamboo/Installer.java
+++ src/main/java/org/netbeans/modules/bamboo/Installer.java
@@ -1,30 +1,41 @@
 package org.netbeans.modules.bamboo;
 
-
-import java.util.Collection;
 import org.netbeans.modules.bamboo.glue.BambooInstance;
 import org.netbeans.modules.bamboo.glue.InstanceManageable;
-import org.openide.util.Lookup;
+import org.netbeans.modules.bamboo.rest.BambooInstanceProduceable;
+import org.netbeans.modules.bamboo.rest.BambooServiceAccessable;
+import org.netbeans.modules.bamboo.rest.DefaultBambooInstance;
+import org.netbeans.modules.bamboo.rest.model.Plans;
+import org.netbeans.modules.bamboo.rest.model.PlansResponse;
+
+import static org.openide.util.Lookup.getDefault;
 
 import org.openide.windows.OnShowing;
+
+import java.util.Collection;
 
 
 @OnShowing
 public final class Installer implements Runnable {
-    
-    private InstanceManageable manager;
-    
-    public Installer() {
-         manager = Lookup.getDefault().lookup(InstanceManageable.class);
-    }
-
-    
     @Override
     public void run() {
+        InstanceManageable manager = getDefault().lookup(InstanceManageable.class);
         Collection<BambooInstance> instances = manager.loadInstances();
-        //TODO call CI server
-        instances.forEach(instance -> manager.addInstance(instance));
-    }
 
-    
+        if (!instances.isEmpty()) {
+            BambooServiceAccessable client = getDefault().lookup(BambooServiceAccessable.class);
+
+            instances.forEach(instance -> {
+                    PlansResponse all = client.getAllPlans(instance);
+                    Plans plans = all.getPlans();
+
+                    if (plans != null) {
+                        //TODO use a dto in the glue and add setter to interface
+                        ((DefaultBambooInstance) instance).setPlans(plans.getPlan());
+                    }
+
+                    manager.addInstance(instance);
+                });
+        }
+    }
 }
