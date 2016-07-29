882146572ea0fe5d2c81810265a95a5223ae4feb
Mario-S <spindizzy@outlook.com>
2016-07-29 13:10:35 +0200

    updated worker

--- src/test/java/org/netbeans/modules/bamboo/ui/wizard/AddInstanceWorkerTest.java
+++ src/test/java/org/netbeans/modules/bamboo/ui/wizard/AddInstanceWorkerTest.java
@@ -1,19 +1,19 @@
 package org.netbeans.modules.bamboo.ui.wizard;
 
-import static org.junit.Assert.*;
-
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import org.junit.runner.RunWith;
 
 import static org.mockito.BDDMockito.given;
 
-import static org.mockito.Matchers.any;
+import org.mockito.InOrder;
+
+import static org.mockito.Matchers.anyString;
 
 import org.mockito.Mock;
 
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.verify;
 
 import static org.mockito.internal.util.reflection.Whitebox.setInternalState;
@@ -21,19 +21,16 @@ import static org.mockito.internal.util.reflection.Whitebox.setInternalState;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import org.netbeans.modules.bamboo.glue.BambooInstance;
-import org.netbeans.modules.bamboo.glue.DefaultInstanceValues;
 import org.netbeans.modules.bamboo.glue.InstanceManageable;
-import org.netbeans.modules.bamboo.glue.InstanceValues;
-import org.netbeans.modules.bamboo.rest.BambooInstanceProduceable;
-import org.netbeans.modules.bamboo.rest.DefaultBambooInstance;
 
-import java.util.Optional;
+import org.openide.util.Task;
+
+import java.beans.PropertyChangeEvent;
 
 
 /**
  * @author spindizzy
  */
-@Ignore("FIXME")
 @RunWith(MockitoJUnitRunner.class)
 public class AddInstanceWorkerTest {
     @Mock
@@ -41,26 +38,44 @@ public class AddInstanceWorkerTest {
     @Mock
     private InstanceManageable instanceManager;
     @Mock
-    private BambooInstanceProduceable bambooInstanceProducer;
-
-    private DefaultBambooInstance instance;
+    private InstancePropertiesForm form;
+    @Mock
+    private BambooInstance instance;
 
     private AddInstanceWorker classUnderTest;
-
-    private final String name;
-
-    public AddInstanceWorkerTest() {
-        name = getClass().getName();
-    }
 
     @Before
     public void setUp() {
         given(action.getInstanceManager()).willReturn(instanceManager);
-
-        instance = new DefaultBambooInstance();
-
         classUnderTest = new AddInstanceWorker(action);
+        given(form.getInstanceName()).willReturn("test");
     }
 
+    @Test
+    public void testExecute_Cancel() {
+        classUnderTest.execute(form);
+        classUnderTest.cancel();
+        verify(form).getPassword();
+    }
 
+    @Test
+    public void testPropertyChangeEvent_InstanceCreated() {
+        PropertyChangeEvent ev =
+            new PropertyChangeEvent(this, WorkerEvents.INSTANCE_CREATED.name(), null, instance);
+        classUnderTest.propertyChange(ev);
+
+        InOrder order = inOrder(instanceManager, action);
+        order.verify(instanceManager).addInstance(instance);
+        order.verify(action).onDone();
+    }
+
+    @Test
+    public void testTaskFinished_Cancel() {
+        classUnderTest.execute(form);
+        setInternalState(classUnderTest, "cancel", true);
+
+        Task task = new Task(null);
+        classUnderTest.taskFinished(task);
+        verify(instanceManager).removeInstance(anyString());
+    }
 }
