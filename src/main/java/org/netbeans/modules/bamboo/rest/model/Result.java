dfde814bcea6d0aee27e61dd669b923fefde6b03
Mario-S <spindizzy@outlook.com>
2016-08-01 16:39:13 +0200

    removed unused builder

--- src/main/java/org/netbeans/modules/bamboo/rest/model/Result.java
+++ src/main/java/org/netbeans/modules/bamboo/rest/model/Result.java
@@ -1,14 +1,15 @@
 package org.netbeans.modules.bamboo.rest.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Getter;
 import lombok.Setter;
+
 
 @Getter
 @Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Result {
-
     private Link link;
     private String key;
     private State state;
@@ -17,37 +18,4 @@ public class Result {
     private int number;
     private int id;
     private String buildReason;
-
-    public static class Builder {
-
-        private final Result result = new Result();
-
-        public Builder number(final int number) {
-            result.number = number;
-
-            return this;
-        }
-
-        public Builder id(final int id) {
-            result.id = id;
-
-            return this;
-        }
-
-        public Builder state(final State state) {
-            result.state = state;
-
-            return this;
-        }
-
-        public Builder lifeCycleState(final LifeCycleState lifeCycleState) {
-            result.lifeCycleState = lifeCycleState;
-
-            return this;
-        }
-
-        public Result build() {
-            return result;
-        }
-    }
 }
