dbd4b12b6f73999e42ed075156352a0844c037e4
Mario-S <spindizzy@outlook.com>
2016-08-19 15:05:54 +0200

    added version to properties

--- src/main/java/org/netbeans/modules/bamboo/ui/nodes/BambooInstanceNode.java
+++ src/main/java/org/netbeans/modules/bamboo/ui/nodes/BambooInstanceNode.java
@@ -24,8 +24,10 @@ import java.util.List;
 import javax.swing.Action;
 import static org.netbeans.modules.bamboo.ui.nodes.Bundle.DESC_Instance_Prop_Name;
 import static org.netbeans.modules.bamboo.ui.nodes.Bundle.DESC_Instance_Prop_Url;
+import static org.netbeans.modules.bamboo.ui.nodes.Bundle.DESC_Instance_Prop_Version;
 import static org.netbeans.modules.bamboo.ui.nodes.Bundle.TXT_Instance_Prop_Name;
 import static org.netbeans.modules.bamboo.ui.nodes.Bundle.TXT_Instance_Prop_Url;
+import static org.netbeans.modules.bamboo.ui.nodes.Bundle.TXT_Instance_Prop_Version;
 import org.openide.nodes.PropertySupport;
 import org.openide.nodes.Sheet;
 import org.openide.util.NbBundle.Messages;
@@ -36,6 +38,7 @@ import org.openide.util.NbBundle.Messages;
  * @author spindizzy
  */
 public class BambooInstanceNode extends AbstractNode implements PropertyChangeListener {
+    private static final String VERSION = "version";
 
     @StaticResource
     private static final String ICON_BASE = "org/netbeans/modules/bamboo/resources/instance.png";
@@ -100,7 +103,9 @@ public class BambooInstanceNode extends AbstractNode implements PropertyChangeLi
         "TXT_Instance_Prop_Name=Name",
         "DESC_Instance_Prop_Name=Bamboo instance name",
         "TXT_Instance_Prop_Url=URL",
-        "DESC_Instance_Prop_Url=Bamboo instance URL"
+        "DESC_Instance_Prop_Url=Bamboo instance URL",
+        "TXT_Instance_Prop_Version=Version",
+        "DESC_Instance_Prop_Version=Bamboo Version"
     })
     private PropertySet getSheetSet() {
         if (sheetSet == null) {
@@ -118,6 +123,13 @@ public class BambooInstanceNode extends AbstractNode implements PropertyChangeLi
                 @Override
                 public String getValue() throws IllegalAccessException, InvocationTargetException {
                     return instance.getUrl();
+                }
+            });
+            
+            sheetSet.put(new StringReadPropertySupport(VERSION, TXT_Instance_Prop_Version(), DESC_Instance_Prop_Version()) {
+                @Override
+                public String getValue() throws IllegalAccessException, InvocationTargetException {
+                    return (instance.getVersion() != null) ? instance.getVersion().getVersion() : "";
                 }
             });
         }
