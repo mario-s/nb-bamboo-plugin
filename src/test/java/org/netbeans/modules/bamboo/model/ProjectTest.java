ad2110847415f7112b98498de294593195ed49ff
Mario-S <spindizzy@outlook.com>
2016-09-08 12:01:47 +0200

    added new test

---
+++ src/test/java/org/netbeans/modules/bamboo/model/ProjectTest.java
@@ -0,0 +1,73 @@
+package org.netbeans.modules.bamboo.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author spindizzy
+ */
+public class ProjectTest {
+    private static final String FOO = "foo";
+    
+    private Project classUnderTest;
+    
+    @Before
+    public void setUp() {
+        classUnderTest = new Project();
+        classUnderTest.setKey(FOO);
+        classUnderTest.setName(FOO);
+    }
+
+    @Test
+    public void testEquals_SameInstance() {
+        assertThat(classUnderTest.equals(classUnderTest), is(true));
+    }
+    
+    @Test
+    public void testEquals_DifferentInstance() {
+        assertThat(classUnderTest.equals(createOther()), is(true));
+    }
+
+    private Project createOther() {
+        Project other = new Project();
+        other.setKey(FOO);
+        other.setName(FOO);
+        Plans plans = new Plans();
+        
+        List<Plan> planList = new ArrayList<>();
+        Plan plan = new Plan();
+        planList.add(plan);
+        plans.setPlan(planList);
+        
+        Result result = new Result();
+        result.setNumber(1);
+        plan.setResult(result);
+        
+        other.setPlans(plans);
+        return other;
+    }
+
+    
+    @Test
+    public void testPlansAsCollection() {
+        Collection<Plan> plans = classUnderTest.plansAsCollection();
+        assertThat(plans.isEmpty(), is(true));
+    }
+    
+    @Test
+    public void testGetUrl() {
+        String url = classUnderTest.getUrl();
+        assertThat(url, containsString("browse"));
+    }
+    
+    
+}
