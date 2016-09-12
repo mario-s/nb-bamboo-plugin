ec55af2e2362be5753ed49bac47903b0683b22e4
Mario-S <spindizzy@outlook.com>
2016-09-12 13:29:48 +0200

    simple status bar, not yet implemented

---
+++ src/main/java/org/netbeans/modules/bamboo/ui/notification/BuildResultStatusBar.java
@@ -0,0 +1,19 @@
+package org.netbeans.modules.bamboo.ui.notification;
+
+import java.awt.Component;
+import org.openide.awt.StatusLineElementProvider;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author spindizzy
+ */
+@ServiceProvider(service = StatusLineElementProvider.class, position=1)
+public class BuildResultStatusBar implements StatusLineElementProvider{
+
+    @Override
+    public Component getStatusLineElement() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+    
+}
