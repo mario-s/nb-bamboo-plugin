2214a2bba341316fff23aa5cb755016c5cfe5d0d
Mario-S <spindizzy@outlook.com>
2016-09-16 16:16:39 +0200

    updated simple status bar

--- src/main/java/org/netbeans/modules/bamboo/ui/notification/BuildResultStatusBar.java
+++ src/main/java/org/netbeans/modules/bamboo/ui/notification/BuildResultStatusBar.java
@@ -1,18 +1,94 @@
 package org.netbeans.modules.bamboo.ui.notification;
 
 import java.awt.Component;
+import java.awt.EventQueue;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Collection;
+import java.util.logging.Level;
+import javax.swing.Icon;
+import javax.swing.JLabel;
+import org.netbeans.api.annotations.common.StaticResource;
+import org.netbeans.modules.bamboo.glue.InstanceManageable;
+import org.netbeans.modules.bamboo.glue.ProjectsProvideable;
+import org.netbeans.modules.bamboo.model.ModelProperties;
+import org.netbeans.modules.bamboo.model.PlanVo;
+import org.netbeans.modules.bamboo.model.ProjectVo;
+import org.netbeans.modules.bamboo.model.ResultVo;
+import org.openide.awt.NotificationDisplayer;
 import org.openide.awt.StatusLineElementProvider;
+import org.openide.util.ImageUtilities;
+import org.openide.util.Lookup;
+import static org.openide.util.Lookup.getDefault;
+import org.openide.util.LookupEvent;
+import org.openide.util.LookupListener;
+import org.openide.util.lookup.ServiceProvider;
 
 /**
  *
  * @author spindizzy
  */
-//@ServiceProvider(service = StatusLineElementProvider.class, position=1)
-public class BuildResultStatusBar implements StatusLineElementProvider{
+@ServiceProvider(service = StatusLineElementProvider.class, position = 1)
+public class BuildResultStatusBar implements StatusLineElementProvider, LookupListener, PropertyChangeListener {
+
+    @StaticResource
+    private static final String ICON_BASE = "org/netbeans/modules/bamboo/resources/instance.png";
+
+    private final InstanceManageable manager;
+
+    private Lookup.Result<ProjectsProvideable> projectResult;
+    
+    private Collection<? extends ProjectsProvideable> projectsProviders;
+    
+
+    public BuildResultStatusBar() {
+        manager = getDefault().lookup(InstanceManageable.class);
+        init();
+    }
+
+    private void init() {
+        projectResult = manager.getLookup().lookupResult(ProjectsProvideable.class);
+        projectResult.addLookupListener(this);
+    }
+
+    private Icon getIcon() {
+        return ImageUtilities.loadImageIcon(ICON_BASE, true);
+    }
 
     @Override
     public Component getStatusLineElement() {
-        throw new UnsupportedOperationException("Not supported yet."); 
+        return null;
     }
-    
+
+    @Override
+    public void resultChanged(LookupEvent ev) {
+        projectsProviders = projectResult.allInstances();
+        registerChangeListener();
+    }
+
+    private void registerChangeListener() {
+        projectsProviders.forEach(provider -> {
+            provider.getProjects().forEach(project -> {
+                project.getPlans().forEach(plan -> {
+                    plan.addPropertyChangeListener(this);
+                });
+            });
+        });
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        if(evt.getPropertyName().equals(ModelProperties.Result.toString())){
+            add((PlanVo) evt.getNewValue());
+        }
+    }
+
+    private void add(PlanVo plan) {
+        EventQueue.invokeLater(() -> {
+            String name = plan.getName();
+            ResultVo resVo = plan.getResult();
+            String details = resVo.getState().toString();
+            NotificationDisplayer.getDefault().notify(name, getIcon(), details, null);
+        });
+    }
 }
