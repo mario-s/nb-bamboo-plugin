deeee232af98074d1222404c6c656df57412a3f9
Mario-S <spindizzy@outlook.com>
2016-09-22 17:40:52 +0200

    updated vo

--- src/main/java/org/netbeans/modules/bamboo/model/PlanVo.java
+++ src/main/java/org/netbeans/modules/bamboo/model/PlanVo.java
@@ -1,9 +1,14 @@
 package org.netbeans.modules.bamboo.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * This class is the plan related to a project.
  * @author spindizzy
  */
+@Getter
+@Setter
 public class PlanVo extends AbstractOpenInBrowserVo{
     
     private String name;
@@ -16,56 +21,11 @@ public class PlanVo extends AbstractOpenInBrowserVo{
     public PlanVo() {
         this.result = new ResultVo();
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getShortKey() {
-        return shortKey;
-    }
-
-    public void setShortKey(String shortKey) {
-        this.shortKey = shortKey;
-    }
-
-    public String getShortName() {
-        return shortName;
-    }
-
-    public void setShortName(String shortName) {
-        this.shortName = shortName;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    public PlanType getType() {
-        return type;
-    }
-
-    public void setType(PlanType type) {
-        this.type = type;
-    }
-
-    public ResultVo getResult() {
-        return result;
-    }
     
     public void setResult(ResultVo result) {
         ResultVo old = this.result;
         this.result = result;
         firePropertyChange(ModelProperties.Result.toString(), old, result);
     }
-    
     
 }
