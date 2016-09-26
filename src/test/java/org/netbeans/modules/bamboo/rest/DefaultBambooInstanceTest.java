d130cf5d86c4988e7d6880e951b6cf3b58ba8942
Mario-S <spindizzy@outlook.com>
2016-09-26 20:02:13 +0200

    updated test

--- src/test/java/org/netbeans/modules/bamboo/rest/DefaultBambooInstanceTest.java
+++ src/test/java/org/netbeans/modules/bamboo/rest/DefaultBambooInstanceTest.java
@@ -3,21 +3,26 @@ package org.netbeans.modules.bamboo.rest;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.Collection;
-import static java.util.Collections.emptyList;
+
 import java.util.prefs.Preferences;
-import static org.hamcrest.CoreMatchers.is;
+import org.junit.After;
+
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
-import static org.mockito.BDDMockito.given;
 import org.mockito.InjectMocks;
-import static org.mockito.Matchers.any;
 import org.mockito.Mock;
-import static org.mockito.Mockito.verify;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.netbeans.modules.bamboo.glue.SharedConstants;
 import org.netbeans.modules.bamboo.model.ProjectVo;
+
+import static org.hamcrest.CoreMatchers.is;
+import static java.util.Collections.emptyList;
+import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 /**
  *
@@ -30,19 +35,28 @@ public class DefaultBambooInstanceTest {
     private BambooInstanceProperties properties;
     @Mock
     private Preferences preferences;
-    @Mock
-    private PropertyChangeListener listener;
+
+    private final PropertyChangeListener listener;
     @InjectMocks
     private DefaultBambooInstance classUnderTest;
-    
+
     private Collection<ProjectVo> projects;
-    
+
+    public DefaultBambooInstanceTest() {
+        listener = mock(PropertyChangeListener.class);
+    }
+
     @Before
     public void setUp() {
         classUnderTest.setSyncInterval(5);
         classUnderTest.addPropertyChangeListener(listener);
         given(properties.getPreferences()).willReturn(preferences);
         projects = emptyList();
+    }
+    
+    @After
+    public void shutDown() {
+        classUnderTest.removePropertyChangeListener(listener);
     }
 
     /**
@@ -64,8 +78,8 @@ public class DefaultBambooInstanceTest {
         classUnderTest.applyProperties(properties);
         assertEquals(5, classUnderTest.getSyncInterval());
     }
-    
-     /**
+
+    /**
      * Test of setProjects method, of class DefaultBambooInstance.
      */
     @Test
@@ -75,11 +89,11 @@ public class DefaultBambooInstanceTest {
         classUnderTest.setProjects(projects);
         assertThat(classUnderTest.getSynchronizationTask().isPresent(), is(true));
     }
-    
+
     @Test
     public void testSynchronize_ListenerShouldBeCalled() throws InterruptedException {
         classUnderTest.synchronize();
-        synchronized (listener){
+        synchronized (listener) {
             listener.wait(1000);
         }
         verify(listener).propertyChange(any(PropertyChangeEvent.class));
