6ba709ac8b754079bbd18e22f908bcfec51ff822
Mario-S <spindizzy@outlook.com>
2016-10-04 16:25:22 +0200

    deleted unused builder from rest pojo

--- src/main/java/org/netbeans/modules/bamboo/model/rest/Results.java
+++ src/main/java/org/netbeans/modules/bamboo/model/rest/Results.java
@@ -2,7 +2,6 @@ package org.netbeans.modules.bamboo.model.rest;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,38 +12,4 @@ import lombok.Setter;
 public class Results extends Metrics {
     private List<Result> result;
     private String expand;
-
-    public static class Builder {
-        private final Results result = new Results();
-
-        public Builder addResult(final Result result) {
-            newResults();
-
-            this.result.result.add(0, result);
-
-            return this;
-        }
-
-        private void newResults() {
-            if (result.result == null) {
-                result.result = new ArrayList<>();
-            }
-        }
-
-        public Results build() {
-            return result;
-        }
-
-        public Builder results(final List<Result> newResults) {
-            result.result = newResults;
-
-            return this;
-        }
-
-        public Builder expand(final String expand) {
-            result.expand = expand;
-
-            return this;
-        }
-    }
 }
