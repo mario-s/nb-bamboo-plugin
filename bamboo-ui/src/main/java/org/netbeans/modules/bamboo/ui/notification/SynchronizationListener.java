7e753ed5f703d5c237c6735ab4b5005bd2537cbb
Mario-S <spindizzy@outlook.com>
2016-10-10 21:15:11 +0200

    updated sync listener

--- bamboo-ui/src/main/java/org/netbeans/modules/bamboo/ui/notification/SynchronizationListener.java
+++ bamboo-ui/src/main/java/org/netbeans/modules/bamboo/ui/notification/SynchronizationListener.java
@@ -2,13 +2,19 @@ package org.netbeans.modules.bamboo.ui.notification;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.Optional;
 import org.netbeans.api.annotations.common.NonNull;
 import org.netbeans.api.progress.ProgressHandle;
 import org.netbeans.api.progress.ProgressHandleFactory;
 import org.netbeans.modules.bamboo.glue.BambooInstance;
 import org.netbeans.modules.bamboo.model.ChangeEvents;
+
 import static org.netbeans.modules.bamboo.ui.notification.Bundle.TXT_SYNC;
+
 import org.openide.util.NbBundle;
+
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
 
 /**
  * This calss listenes for changes on the sychronization.
@@ -19,10 +25,15 @@ class SynchronizationListener implements PropertyChangeListener {
 
     private final BambooInstance instance;
 
-    private ProgressHandle progressHandle;
+    private Optional<ProgressHandle> progressHandle;
 
     SynchronizationListener(@NonNull BambooInstance instance) {
         this.instance = instance;
+        progressHandle = empty();
+        init();
+    }
+
+    private void init() {
         instance.addPropertyChangeListener(this);
     }
 
@@ -30,27 +41,39 @@ class SynchronizationListener implements PropertyChangeListener {
     public void propertyChange(PropertyChangeEvent evt) {
         String propertyName = evt.getPropertyName();
         if (ChangeEvents.Synchronizing.toString().equals(propertyName)) {
-            boolean progress = (boolean) evt.getNewValue();
-            progressHandle = getProgressHandle();
-            if(progress){
-                progressHandle.start();
-            }else{
-                progressHandle.finish();
-                progressHandle = null;
+            boolean sync = (boolean) evt.getNewValue();
+            if (sync) {
+                startProgress();
+            } else {
+                finishProgress();
             }
         }
     }
 
-    @NbBundle.Messages({"TXT_SYNC=Synchronizing"})
-    private ProgressHandle getProgressHandle() {
-        if(progressHandle == null){
-            progressHandle = ProgressHandleFactory.createHandle(TXT_SYNC() + " " + getName());
+    private void startProgress() {
+        getProgressHandle().get().start();
+    }
+
+    private void finishProgress() {
+        if (progressHandle.isPresent()) {
+            progressHandle.get().finish();
+            progressHandle = empty();
+        }
+    }
+
+    private Optional<ProgressHandle> getProgressHandle() {
+        if (!progressHandle.isPresent()) {
+            progressHandle = of(newProgressHandle());
         }
         return progressHandle;
+    }
+
+    @NbBundle.Messages({"TXT_SYNC=Synchronizing"})
+    private ProgressHandle newProgressHandle() {
+        return ProgressHandleFactory.createHandle(TXT_SYNC() + " " + getName());
     }
 
     private String getName() {
         return instance.getName();
     }
-
 }
---
+++ bamboo-ui/src/test/java/org/netbeans/modules/bamboo/ui/notification/SynchronizationListenerTest.java
@@ -0,0 +1,66 @@
+package org.netbeans.modules.bamboo.ui.notification;
+
+import java.beans.PropertyChangeEvent;
+import java.util.Optional;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.netbeans.api.progress.ProgressHandle;
+import org.netbeans.modules.bamboo.glue.BambooInstance;
+import org.netbeans.modules.bamboo.model.ChangeEvents;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.mockito.internal.util.reflection.Whitebox.getInternalState;
+
+/**
+ *
+ * @author spindizzy
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class SynchronizationListenerTest {
+    @Mock
+    private BambooInstance instance;
+    
+    private SynchronizationListener classUnderTest;
+    
+    @Before
+    public void setUp() {
+        classUnderTest = new SynchronizationListener(instance);
+    }
+
+    /**
+     * Test of propertyChange method, of class SynchronizationListener.
+     */
+    @Test
+    public void testPropertyChange_StartSynchronize() {
+        PropertyChangeEvent event = newChangeEvent(true);
+        classUnderTest.propertyChange(event);
+        Optional<ProgressHandle> opt = getProgressHandle();
+        assertThat(opt.isPresent(), is(true));
+    }
+
+    private PropertyChangeEvent newChangeEvent(boolean val) {
+        return new PropertyChangeEvent(this, ChangeEvents.Synchronizing.toString(), !val, val);
+    }
+    
+      /**
+     * Test of propertyChange method, of class SynchronizationListener.
+     */
+    @Test
+    public void testPropertyChange_StartAndStopSynchronize() {
+        PropertyChangeEvent event = newChangeEvent(true);
+        classUnderTest.propertyChange(event);
+        event = newChangeEvent(false);
+        classUnderTest.propertyChange(event);
+        Optional<ProgressHandle> opt = getProgressHandle();
+        assertThat(opt.isPresent(), is(false));
+    }
+
+    private Optional<ProgressHandle> getProgressHandle() {
+        return (Optional<ProgressHandle>) getInternalState(classUnderTest, "progressHandle");
+    }
+    
+}
