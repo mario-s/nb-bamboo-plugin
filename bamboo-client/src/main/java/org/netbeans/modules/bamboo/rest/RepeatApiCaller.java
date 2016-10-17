0657e1f2113f18d4e80ea6036f0a735fb8a1004d
Mario-S <spindizzy@outlook.com>
2016-10-17 21:18:09 +0200

    updated doc

--- bamboo-client/src/main/java/org/netbeans/modules/bamboo/rest/RepeatApiCaller.java
+++ bamboo-client/src/main/java/org/netbeans/modules/bamboo/rest/RepeatApiCaller.java
@@ -14,7 +14,7 @@ import static java.util.Optional.of;
 import lombok.extern.java.Log;
 
 /**
- * This class can be used to perform a second call to the REST API. It is generall used in cases where we don't know the
+ * This class can be used to perform a second call to the REST API. It is used in cases where we don't know the
  * maximum of expected result upfront.
  *
  * @author spindizzy
