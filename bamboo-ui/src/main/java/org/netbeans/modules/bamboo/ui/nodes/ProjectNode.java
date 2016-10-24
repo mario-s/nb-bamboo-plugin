79f7202a7e445c90a040220bc3adb6fff6a65efe
Mario-S <spindizzy@outlook.com>
2016-10-24 22:56:53 +0200

    cleanup

--- bamboo-ui/src/main/java/org/netbeans/modules/bamboo/ui/nodes/ProjectNode.java
+++ bamboo-ui/src/main/java/org/netbeans/modules/bamboo/ui/nodes/ProjectNode.java
@@ -36,7 +36,6 @@ public class ProjectNode extends AbstractInstanceChildNode {
 
     @StaticResource
     private static final String FOLDER = "org/netbeans/modules/bamboo/resources/folder.png";
-    protected static final String NO_CONTROL_SHADOW = "<font color='!controlShadow'>(%s)</font>";
 
     private final ProjectVo project;
     
