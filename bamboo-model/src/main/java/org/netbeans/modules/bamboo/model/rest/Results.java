a930ce2ecee010245c5d3150fae4ff2d2ce81010
Mario-S <spindizzy@outlook.com>
2016-10-31 10:26:57 +0100

    root element for results

--- bamboo-model/src/main/java/org/netbeans/modules/bamboo/model/rest/Results.java
+++ bamboo-model/src/main/java/org/netbeans/modules/bamboo/model/rest/Results.java
@@ -3,12 +3,14 @@ package org.netbeans.modules.bamboo.model.rest;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.util.List;
+import javax.xml.bind.annotation.XmlRootElement;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
+@XmlRootElement
 public class Results extends Metrics {
     private List<Result> result;
     private String expand;
