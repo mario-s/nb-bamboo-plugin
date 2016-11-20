057510ab8f5d6d273de23dec534a1883838bff82
Mario-S <spindizzy@outlook.com>
2016-11-20 19:03:17 +0100

    moved property support

--- bamboo-ui/src/main/java/org/netbeans/modules/bamboo/ui/nodes/AbstractInstanceChildNode.java
+++ bamboo-ui/src/main/java/org/netbeans/modules/bamboo/ui/nodes/AbstractInstanceChildNode.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.swing.Action;
 import org.openide.nodes.AbstractNode;
 import org.openide.nodes.Children;
+import org.openide.nodes.PropertySupport;
 import org.openide.util.Lookup;
 
 import static org.openide.util.Utilities.actionsForPath;
@@ -24,4 +25,47 @@ abstract class AbstractInstanceChildNode extends AbstractNode implements Propert
         return actionsForPath(path);
     }
 
+    /**
+     * Abstract parent for read only support of strings.
+     */
+    abstract class StringReadPropertySupport extends PropertySupport.ReadOnly<String> {
+
+        public StringReadPropertySupport(String name, String displayName, String shortDescription) {
+            super(name, String.class, displayName, shortDescription);
+        }
+
+    }
+
+    /**
+     * Abstract parent for read only support of integers.
+     */
+    abstract class IntReadPropertySupport extends PropertySupport.ReadOnly<Integer> {
+
+        public IntReadPropertySupport(String name, String displayName, String shortDescription) {
+            super(name, Integer.class, displayName, shortDescription);
+        }
+
+    }
+
+    /**
+     * Abstract parent for read and write support of integers.
+     */
+    abstract class IntReadWritePropertySupport extends PropertySupport.ReadWrite<Integer> {
+
+        public IntReadWritePropertySupport(String name, String displayName, String shortDescription) {
+            super(name, Integer.class, displayName, shortDescription);
+        }
+
+    }
+
+    /**
+     * Abstract parent for read and write support of boolean.
+     */
+    abstract class BooleanReadWritePropertySupport extends PropertySupport.ReadWrite<Boolean> {
+
+        public BooleanReadWritePropertySupport(String name, String displayName, String shortDescription) {
+            super(name, Boolean.class, displayName, shortDescription);
+        }
+
+    }
 }
