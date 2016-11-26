a8abb38a01b8aed54d6f4926425982dde05d9fd6
Mario-S <spindizzy@outlook.com>
2016-11-26 19:03:55 +0100

    added javadoc

--- bamboo-client/src/main/java/org/netbeans/modules/bamboo/glue/InstanceManageable.java
+++ bamboo-client/src/main/java/org/netbeans/modules/bamboo/glue/InstanceManageable.java
@@ -6,22 +6,56 @@ import org.openide.util.lookup.InstanceContent;
 
 import java.util.Collection;
 
-
 /**
+ * This interface provides methods to to deal with {@link BambooInstance}.
+ *
  * @author spindizzy
  */
 public interface InstanceManageable extends Lookup.Provider {
+
     InstanceContent getContent();
 
+    /**
+     * This method adds a new instance to the manager.
+     *
+     * @param instance the given instance to save
+     */
     void addInstance(final BambooInstance instance);
 
+    /**
+     * This method removes the instance from the manager and user's preferences.
+     *
+     * @param name the name of the instance to remove
+     */
     void removeInstance(final String name);
 
+    /**
+     * This method removes the instance from the manager and user's preferences.
+     *
+     * @param instance the instance to remove
+     */
     void removeInstance(final BambooInstance instance);
 
+    /**
+     * This method returns <code>true</code> when an instance with the given name exist in the user's preferences, if
+     * not <code>false</coe>.
+     *
+     * @param name the name of the instance
+     * @return boolean
+     */
     boolean existsInstance(final String name);
 
+    /**
+     * Load all persisted instances from the user's preferences.
+     *
+     * @return a collection of the instances
+     */
     Collection<BambooInstance> loadInstances();
 
+    /**
+     * Persist the instance in the user's preferences.
+     *
+     * @param instance the instance to save
+     */
     void persist(BambooInstance instance);
 }
