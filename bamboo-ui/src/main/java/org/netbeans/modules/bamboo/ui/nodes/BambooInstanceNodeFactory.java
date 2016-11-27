4f129ca76712ae23848041c4defedee1a148ee8f
Mario-S <spindizzy@outlook.com>
2016-11-27 10:15:06 +0100

    updated factory to use countdown

--- bamboo-ui/src/main/java/org/netbeans/modules/bamboo/ui/nodes/BambooInstanceNodeFactory.java
+++ bamboo-ui/src/main/java/org/netbeans/modules/bamboo/ui/nodes/BambooInstanceNodeFactory.java
@@ -15,31 +15,38 @@ import static java.util.Collections.sort;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
 import lombok.extern.java.Log;
 import org.netbeans.modules.bamboo.glue.InstanceManageable;
 import org.netbeans.modules.bamboo.model.event.InstancesLoadEvent;
-import org.openide.util.Exceptions;
 
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
 
 /**
  * This is a factory for a {@link BambooInstanceNode}.
- * 
+ *
  * @author spindizzy
  */
 @Log
 class BambooInstanceNodeFactory extends ChildFactory<BambooInstance>
-    implements LookupListener {
+        implements LookupListener {
+
     private static final long SEC = 1000l;
-    
+
     private static final BambooInstanceComparator COMPARATOR = new BambooInstanceComparator();
-    
+
     private final Lookup lookup;
-    
+
     private Lookup.Result<BambooInstance> instanceResult;
-    
+
     private Lookup.Result<InstancesLoadEvent> eventResult;
 
+    private Optional<CountDownLatch> blocker;
+
     public BambooInstanceNodeFactory(InstanceManageable manager) {
+        this.blocker = empty();
         this.lookup = manager.getLookup();
         lookupResult();
     }
@@ -57,49 +64,51 @@ class BambooInstanceNodeFactory extends ChildFactory<BambooInstance>
 
     @Override
     protected boolean createKeys(final List<BambooInstance> toPopulate) {
-        
-        try {
-            tryToWait();
-        } catch (InterruptedException ex) {
-            log.info(ex.getMessage());
-        }
-        
+
+        blocker.ifPresent(c -> {
+            try {
+                c.await();
+            } catch (InterruptedException ex) {
+                log.info(ex.getMessage());
+            }
+        });
+
         toPopulate.addAll(instanceResult.allInstances());
         sort(toPopulate, COMPARATOR);
-        
+
+        blocker = empty();
+
         return true;
     }
-    
-    private void tryToWait() throws InterruptedException {
-        Collection<? extends InstancesLoadEvent> events = eventResult.allInstances();
-        if(!events.isEmpty()){
-            int loadSize = events.size();
-            int syncSize;
-            do {
-                syncSize = instanceResult.allInstances().size();
-                Thread.sleep(SEC);
-            }while(loadSize != syncSize);
-        }
-    }
-    
 
     @Override
     public void resultChanged(final LookupEvent ev) {
+        Collection<? extends InstancesLoadEvent> events = eventResult.allInstances();
+        if (!events.isEmpty() && !blocker.isPresent()) {
+            CountDownLatch countDown = new CountDownLatch(events.size());
+            blocker = of(countDown);
+        }
+
+        Collection<? extends BambooInstance> instances = instanceResult.allInstances();
+        if(!instances.isEmpty()) {
+            blocker.ifPresent(c -> instances.forEach(i -> c.countDown()));
+        }
+        
         refresh(false);
     }
 
     private static class BambooInstanceComparator implements Comparator<BambooInstance>, Serializable {
 
         private static final long serialVersionUID = 1L;
-        
+
         @Override
         public int compare(final BambooInstance o1, final BambooInstance o2) {
             int val = 0;
-            
+
             final String left = o1.getName();
             final String right = o2.getName();
-            
-            if(left != null && right != null){
+
+            if (left != null && right != null) {
                 val = left.compareToIgnoreCase(right);
             }
             return val;
