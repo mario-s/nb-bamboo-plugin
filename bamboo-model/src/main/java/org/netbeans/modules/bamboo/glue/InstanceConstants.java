57372f629568ec549e490bcd973b7ae04992f309
Mario-S <spindizzy@outlook.com>
2016-11-29 16:56:09 +0100

    new constant for silent plans

--- bamboo-model/src/main/java/org/netbeans/modules/bamboo/glue/InstanceConstants.java
+++ bamboo-model/src/main/java/org/netbeans/modules/bamboo/glue/InstanceConstants.java
@@ -5,11 +5,24 @@ package org.netbeans.modules.bamboo.glue;
  */
 public interface InstanceConstants {
     
-    
+    /**
+     * The name of the CI instance.
+     */
     String PROP_NAME = "name";
     
+    /**
+     * THe url to the server.
+     */
     String PROP_URL = "url";
     
+    /**
+     * The synchronization interval.
+     */
     String PROP_SYNC_INTERVAL = "syncInterval";
+    
+    /**
+     * Nonsalient plans for the instance, list of job names, separated by |.
+     */
+    String SUPPRESSED_PLANS = "suppressed_plans";
     
 }
