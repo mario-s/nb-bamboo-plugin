80e84f3b7edb9395c0cd8616325441f0ce791108
Mario-S <spindizzy@outlook.com>
2017-01-03 23:01:36 +0100

    updated converter

--- bamboo-model/src/main/java/org/netbeans/modules/bamboo/model/convert/AbstractVoConverter.java
+++ bamboo-model/src/main/java/org/netbeans/modules/bamboo/model/convert/AbstractVoConverter.java
@@ -1,11 +1,7 @@
 package org.netbeans.modules.bamboo.model.convert;
 
 import java.time.LocalDateTime;
-import java.util.Collection;
-import static java.util.Collections.emptyList;
 import java.util.Optional;
-import static java.util.stream.Collectors.toList;
-import org.netbeans.modules.bamboo.model.rest.Responseable;
 
 /**
  *
@@ -16,15 +12,5 @@ abstract class AbstractVoConverter<S, T> implements VoConverter<S, T> {
     protected Optional<LocalDateTime> toDate(String str) {
         return new LocalDateTimeConverter().convert(str);
     }
-    
-    protected <S, T> Collection<T> convert(final Responseable<S> source, VoConverter<S, T> converter) {
-        return (source != null) ? convert(source.asCollection(), converter) : emptyList();
-    }
-
-    protected <S, T> Collection<T> convert(final Collection<S> collection, VoConverter<S, T> converter) {
-        return collection.stream().map(
-                c -> {
-                    return converter.convert(c);
-                }).collect(toList());
-    }
+   
 }
--- bamboo-model/src/main/java/org/netbeans/modules/bamboo/model/convert/CollectionVoConverter.java
+++ bamboo-model/src/main/java/org/netbeans/modules/bamboo/model/convert/CollectionVoConverter.java
@@ -3,24 +3,38 @@ package org.netbeans.modules.bamboo.model.convert;
 import java.util.Collection;
 import org.netbeans.modules.bamboo.model.rest.Responseable;
 
+import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toList;
 
 /**
  * A converter for collection.
+ *
  * @author spindizzy
  * @param <S> the source
  * @param <T> the target
  */
-public class CollectionVoConverter<S extends Responseable, T> extends AbstractVoConverter <S, Collection<T>>{
-    
+public class CollectionVoConverter<S extends Responseable, T> extends AbstractVoConverter<S, Collection<T>> {
+
     private final VoConverter<S, T> converter;
 
     public CollectionVoConverter(VoConverter<S, T> converter) {
         this.converter = converter;
     }
-    
+
     @Override
     public Collection<T> convert(S src) {
         return convert(src, converter);
     }
-    
+
+    private <S, T> Collection<T> convert(final Responseable<S> source, VoConverter<S, T> converter) {
+        return (source != null) ? convert(source.asCollection(), converter) : emptyList();
+    }
+
+    private <S, T> Collection<T> convert(final Collection<S> collection, VoConverter<S, T> converter) {
+        return collection.stream().map(
+                c -> {
+                    return converter.convert(c);
+                }).collect(toList());
+    }
+
 }
