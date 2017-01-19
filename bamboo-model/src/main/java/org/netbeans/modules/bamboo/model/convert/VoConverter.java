a2885e8f05e98f838a2311063bce93b4745b1774
Mario-S <spindizzy@outlook.com>
2017-01-20 00:40:28 +0100

    [RELEASE]squashing 'release-1.0.0' into 'master'

--- bamboo-model/src/main/java/org/netbeans/modules/bamboo/model/convert/VoConverter.java
+++ bamboo-model/src/main/java/org/netbeans/modules/bamboo/model/convert/VoConverter.java
@@ -1,28 +1,5 @@
 package org.netbeans.modules.bamboo.model.convert;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatterBuilder;
-import java.time.format.DateTimeParseException;
-import java.util.Collection;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import lombok.extern.java.Log;
-import org.netbeans.modules.bamboo.model.rcp.ChangeVo;
-import org.netbeans.modules.bamboo.model.rcp.PlanVo;
-import org.netbeans.modules.bamboo.model.rcp.ProjectVo;
-import org.netbeans.modules.bamboo.model.rcp.ResultVo;
-import org.netbeans.modules.bamboo.model.rcp.VersionInfo;
-import org.netbeans.modules.bamboo.model.rest.Change;
-import org.netbeans.modules.bamboo.model.rest.Changes;
-import org.netbeans.modules.bamboo.model.rest.Info;
-import org.netbeans.modules.bamboo.model.rest.Plan;
-import org.netbeans.modules.bamboo.model.rest.Project;
-import org.netbeans.modules.bamboo.model.rest.Result;
-
-import static java.util.Optional.empty;
-import static java.util.Optional.of;
-import static org.apache.commons.lang3.StringUtils.isNotBlank;
-
 /**
  * Interface for class which convert class from the rest model to class for model to be used for the view.
  *
