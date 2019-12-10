1c429da1c35a7c99419956e120d8cd7b57921920
Mario-S <spindizzy@outlook.com>
2019-12-10 14:55:06 +0100

    remove lombok from link

--- bamboo-model/src/main/java/org/netbeans/modules/bamboo/model/rest/Link.java
+++ bamboo-model/src/main/java/org/netbeans/modules/bamboo/model/rest/Link.java
@@ -13,16 +13,61 @@
  */
 package org.netbeans.modules.bamboo.model.rest;
 
+import java.util.Objects;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
-import lombok.Data;
 
-@Data
+/**
+ * A link to a result, plan or project.
+ */
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Link {
     @XmlAttribute
     private String href;
     @XmlAttribute
     private String rel;
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    public String getRel() {
+        return rel;
+    }
+
+    public void setRel(String rel) {
+        this.rel = rel;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 83 * hash + Objects.hashCode(this.href);
+        hash = 83 * hash + Objects.hashCode(this.rel);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Link other = (Link) obj;
+        if (!Objects.equals(this.href, other.href)) {
+            return false;
+        }
+        return Objects.equals(this.rel, other.rel);
+    }
+       
 }
