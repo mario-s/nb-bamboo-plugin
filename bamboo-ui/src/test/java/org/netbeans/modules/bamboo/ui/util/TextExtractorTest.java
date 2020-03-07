49615e0d26fb2029bc6ab8b385dce4355f5e696b
Mario-S <spindizzy@outlook.com>
2020-03-07 23:40:11 +0100

    migrate more tests

--- bamboo-ui/src/test/java/org/netbeans/modules/bamboo/ui/util/TextExtractorTest.java
+++ bamboo-ui/src/test/java/org/netbeans/modules/bamboo/ui/util/TextExtractorTest.java
@@ -14,24 +14,24 @@
 package org.netbeans.modules.bamboo.ui.util;
 
 import java.util.List;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 /**
  *
  * @author Mario Schroeder
  */
-public class TextExtractorTest {
+class TextExtractorTest {
 
     private TextExtractor classUnderTest;
 
-    @Before
-    public void setUp() {
+    @BeforeEach
+    void setUp() {
         classUnderTest = new TextExtractor();
     }
 
@@ -39,63 +39,61 @@ public class TextExtractorTest {
      * Test of containsLink method, of class TextExtractor.
      */
     @Test
-    public void testContainsLink_HtmlString_ExpectTrue() {
+    void testContainsLink_HtmlString_ExpectTrue() {
         String text = "test <a href=\"http://localhost\">test</a>";
-        boolean result = classUnderTest.containsLink(text);
-        assertThat(result, is(true));
+        assertTrue(classUnderTest.containsLink(text));
     }
 
     /**
      * Test of containsLink method, of class TextExtractor.
      */
     @Test
-    public void testContainsLink_NormalString_ExpectFalse() {
+    void testContainsLink_NormalString_ExpectFalse() {
         String text = "test";
-        boolean result = classUnderTest.containsLink(text);
-        assertThat(result, is(false));
+        assertFalse(classUnderTest.containsLink(text));
     }
 
     /**
      * Test to remove all tags
      */
     @Test
-    public void testRemoveTags_SimpleLink_ShouldBeNoMarkup() {
+    void testRemoveTags_SimpleLink_ShouldBeNoMarkup() {
         String text = "Manual run from: <b>Foo, Bar</b> <a href=\"https://localhost\">by</a>";
         String expected = "Manual run from: Foo, Bar by";
         String result = classUnderTest.removeTags(text);
-        assertThat(result, equalTo(expected));
+        assertEquals(expected, result);
     }
     
     /**
      * Test to remove all tags
      */
     @Test
-    public void testRemoveTags_LongLink_ShouldBeNoMarkup() {
+    void testRemoveTags_LongLink_ShouldBeNoMarkup() {
         String text = "Changes by <a href=\"http://192.168.99.100:32771/authors/viewAuthor.action?authorName=Foo-BarS%20%3Cfoo%40bar.com%3E\">Foo-Bar &lt;foo@bar.com&gt;</a>";
         String expected = "Changes by Foo-Bar <foo@bar.com>";
         String result = classUnderTest.removeTags(text);
-        assertThat(result, equalTo(expected));
+        assertEquals(expected, result);
     }
 
     @Test
-    public void testFindAllLinks_TwoLinks_ExpectTwo() {
+    void testFindAllLinks_TwoLinks_ExpectTwo() {
         String text = "test <a href=\"http://localhost\">test</a> and <a href=\"https://localhost\">test</a>";
         List<String> result = classUnderTest.extractLinks(text);
-        assertThat(result.size(), is(2));
+        assertEquals(2, result.size());
     }
 
     @Test
-    public void testFindAllLinks_Empty_ExpectEmpty() {
+    void testFindAllLinks_Empty_ExpectEmpty() {
         String text = "";
         List<String> result = classUnderTest.extractLinks(text);
-        assertThat(result.isEmpty(), is(true));
+        assertTrue(result.isEmpty());
     }
     
     @Test
-    public void testreplaceLinksWithText_TwoLinks_ExpectNone() {
+    void testreplaceLinksWithText_TwoLinks_ExpectNone() {
         String text = "test <b/> <a href=\"http://localhost\">test</a> and <a href=\"https://localhost\">test</a>";
         String result = classUnderTest.replaceLinksWithText(text);
         String expected = "test <b/> test and test";
-        assertThat(result, equalTo(expected));
+        assertEquals(expected, result);
     }
 }
